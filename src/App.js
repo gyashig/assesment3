16f5317f4d461db01686b19143dd8188c1ee696e
Yashi Gupta <yashi.12019103@lpu.in>
2023-02-15 21:12:54 +0530

    counter-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  let color = 'green';
+  if (count >= 5 && count <= 9) {
+    color = 'blue';
+  } else if (count === 10) {
+    color = 'red';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:'center'}}>
+      <h1 style={{ color: color }}>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
-
 export default App;
